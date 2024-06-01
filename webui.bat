f594a35b11d4a2e1c624b166f6ca6a161b06c777
olegchomp <4omp@liveinternet.ru>
2024-06-01 18:08:49 +0300

    Fix for multiple python versions

--- webui.bat
+++ webui.bat
@@ -1,47 +1,14 @@
 @echo off
 
+set PYTHON_PATH=
+
 REM Check if Git is installed
 where git > nul 2>&1
 if %errorlevel% neq 0 (
     echo Git is not installed or not found in the system PATH.
     pause
     exit /b 1
-) else (
-    echo Git is installed.
-)
-
-REM Set default Python path manually if needed
-set PYTHON_PATH=
-
-REM Find the Python executable path
-for /f "delims=" %%i in ('where python') do set PYTHON_PATH=%%i
-
-REM Check if Python executable is found
-if "%PYTHON_PATH%" neq "" (
-    REM Extract Python version string
-    for /f "tokens=2 delims= " %%v in ('"%PYTHON_PATH%" --version 2^>^&1') do (
-        set PYTHON_VERSION=%%v
-        echo Found Python version: %%v
-
-        REM Split version based on dot and get first two elements
-        for /f "tokens=1,2 delims=." %%a in ("%%v") do (
-            set PYTHON_MINOR_VERSION=%%b
-
-            REM Check if minor version is 11
-            if "%%b" equ "11" (
-                echo Python path: %PYTHON_PATH%
-            ) else (
-                echo Python version is not 3.11 or later.
-                pause
-                exit /b 1
-            )
-        )
-    )
-) else (
-    echo Python is not installed.
-    pause
-    exit /b 1
-)
+) 
 
 if not exist .venv (
     echo Creating .venv directory...
